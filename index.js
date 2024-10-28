f8d61267273b1a2c388328c0b36272b92a3733e7
Csilla Toth <csillaout@gmail.com>
2024-10-28 16:09:58 +0000

    api

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //require("dotenv").config();
-const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=M1Sc5YaIqGuJ9QWS3GUtYMvYY6HnLYuY&&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=M1Sc5YaIqGuJ9QWS3GUtYMvYY6HnLYuY&q=dog&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
 
 // Print out value of API key stored in .env file
 //console.log(process.env.API_KEY);
