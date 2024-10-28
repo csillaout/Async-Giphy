db7157ff07fe1c750386b9a897b175fe7949061e
Csilla Toth <csillaout@gmail.com>
2024-10-28 16:06:16 +0000

    csillas version

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-require('dotenv').config();
+//require("dotenv").config();
+const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=M1Sc5YaIqGuJ9QWS3GUtYMvYY6HnLYuY&&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
 
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+//console.log(process.env.API_KEY);
+
+async function getImage(query) {
+  const response = await fetch(endpoint);
+  const parsed = await response.json();
+  return parsed.data[0].url;
+}
+getImage("dog").then(function (result) {
+  console.log(result);
+});
